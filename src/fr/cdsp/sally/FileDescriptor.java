875147d66e7f846e82d3b01b54e646b58185fc82
Raphaël Paoloni <raphael.paoloni@sciencespo.fr>
2015-06-12 16:01:36 +0200

    license (GPLv3)

--- src/fr/cdsp/sally/FileDescriptor.java
+++ src/fr/cdsp/sally/FileDescriptor.java
@@ -26,6 +26,24 @@ import org.apache.pdfbox.preflight.parser.PreflightParser;
 
 import fr.cdsp.sally.Constants;
 import fr.cdsp.sally.UnreadableFileException;
+/*
+	Copyright 2015 
+	Centre de données socio-politiques (CDSP)
+	Fondation nationale des sciences politiques (FNSP)
+	Centre national de la recherche scientifique (CNRS)
+License
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 
 /**
  * classe servant simplement a représenter un fichier sous une forme plus utilisable et avec toutes les 
